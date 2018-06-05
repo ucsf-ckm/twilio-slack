240d45c303bba8bfbf2abbba2de5f8d116445bb6
Rich Trott <rtrott@gmail.com>
2018-06-05 16:17:40 -0700

    more tests

--- aws-lambda.test.js
+++ aws-lambda.test.js
@@ -3,9 +3,50 @@
 const { handler } = require('./aws-lambda');
 
 test('requires a Slack token', done => {
+  delete process.env.SLACK_VALIDATION_TOKEN;
+  delete process.env.SLACK_CHANNEL_ID;
+
   handler({body: ''}, {}, (err, response) => {
     expect(err).toBe(null);
     expect(JSON.parse(response.body)).toEqual({text: 'Message not sent: Invalid Slack token.'});
+    done();
+  });
+});
+
+test('Slack token must match', done => {
+  process.env.SLACK_VALIDATION_TOKEN = 'fhqwhgads';
+  delete process.env.SLACK_CHANNEL_ID;
+
+  handler({body: 'token=sterrance'}, {}, (err, response) => {
+    expect(err).toBe(null);
+    expect(JSON.parse(response.body)).toEqual({text: 'Message not sent: Invalid Slack token.'});
+    done();
+  });
+});
+
+test('channel_id must match if one is set', done => {
+  process.env.SLACK_VALIDATION_TOKEN = 'fhqwhgads';
+  process.env.SLACK_CHANNEL_ID = 'cannonmouth';
+
+  handler({body: 'token=fhqwhgads&channel_id=sterrance'}, {}, (err, response) => {
+    expect(err).toBe(null);
+    expect(JSON.parse(response.body)).toEqual({text: 'Sorry! This command is not permitted in this channel!'});
+    done();
+  });
+
+  handler({body: 'token=fhqwhgads&channel_id=cannonmouth'}, {}, (err, response) => {
+    expect(err).toBe(null);
+    expect(JSON.parse(response.body)).not.toEqual({text: 'Sorry! This command is not permitted in this channel!'});
+  });
+});
+
+test('requires a phone number', done => {
+  process.env.SLACK_VALIDATION_TOKEN = 'fhqwhgads';
+  delete process.env.SLACK_CHANNEL_ID;
+
+  handler({body: 'token=fhqwhgads'}, {}, (err, response) => {
+    expect(err).toBe(null);
+    expect(JSON.parse(response.body)).toEqual({text: 'Message not sent: Phone number must be in +1XXXXXXXXXX format: ""'});
     done();
   });
 });
